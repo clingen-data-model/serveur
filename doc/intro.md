448215b43274dcf762968222c2966dbf0f20dc15
Tristan Nelson <tristan@iseult.us>
2017-04-14 11:40:35 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to serveur
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/serveur/core.clj
@@ -0,0 +1,7 @@
+(ns serveur.core
+  (:require [neoclj.core :as neo]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
