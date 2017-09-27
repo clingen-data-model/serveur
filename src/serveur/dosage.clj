750d9325c0eeec444ac4826b03994b050206d800
Tristan Nelson <tristan@iseult.us>
2017-09-27 18:45:38 -0400

    Successfully updating data exchange

--- src/serveur/dosage.clj
+++ src/serveur/dosage.clj
@@ -9,7 +9,7 @@
   ;; (spit (str "./log/" (re-find #"[\w-]+$" (.key record))) record)
   (let [interp (-> record .value json/parse-string)]
     (println "importing: " (interp "iri"))
-    (.run session "merge "))) ; TODO start here, do neo import statement
+    (.run session "match (g:Gene {iri: $gene}), (i:RDFClass {iri: $interpretation}) merge (a:GeneDosageAssertion:Assertion:Entity {iri: $iri}) set a.date = $modified merge (a)-[:has_subject]->(g) merge (a)-[:has_predicate]->(i) with a match (d:RDFClass {iri: $phenotype}) merge (a)-[:has_object]->(d)" interp)))
 
 (defn update-loop
   "Poll kafka for updates and import to knowledge base"
